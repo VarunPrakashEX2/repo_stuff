225b9ce4cad7f74a4d53e9ede825289e2fb6122b
Varun Prakash <98808045+VarunPrakashEX2@users.noreply.github.com>
2023-11-10 21:46:49 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+from starlette.config import Config
+from starlette.datastructures import Secret
+
+config = Config(".env")
+
+# API
+TITLE: str = config("TITLE", cast=str)
+DESCRIPTION: str = config("DESCRIPTION", cast=str)
+VERSION: str = config("VERSION", cast=str)
+
+# OpenAI
+OPENAI_API_KEY: Secret = config("OPENAI_KEY", cast=Secret)
+OPENAI_MODEL: str = config("OPENAI_MODEL_NAME", cast=str)
+
+# Backend API
+BACKEND_API_URL: str = config("BACKEND_API_URL", cast=str)
+QA_ENDPOINT: str = config("QA_ENDPOINT", cast=str)
+DOCUMENT_LIST_ENDPOINT: str = config("DOCUMENT_LIST_ENDPOINT", cast=str)
+INGEST_FILE_ENDPOINT: str = config("INGEST_FILE_ENDPOINT", cast=str)
+
+# Application Credentials
+ADMIN_EMAIL: str = config("ADMIN_EMAIL", cast=str)
